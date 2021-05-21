cd034df57e2a04a980724aafde4b1fb61e6068c8
Valentin NOEL <valentin.noel@media-io.com>
2021-05-21 17:09:17 +0200

    Simplify rendering C API into a single function
    To simplify objects (de)allocation

--- src/adm_engine/adm_engine.cpp
+++ src/adm_engine/adm_engine.cpp
@@ -26,68 +26,6 @@ void _assign_string_to_pointer(const std::string& str, const char** pointer) {
   std::memcpy((void*)*pointer, str.c_str(), length);
 }
 
-int _dump_bw64_adm_file(const std::string& inputFilePath, const char** output) {
-  try {
-    auto bw64File = bw64::readFile(inputFilePath);
-
-    const std::string admDocumentStr = getAdmDocumentAsString(getAdmDocument(parseAdmXmlChunk(bw64File)));
-
-    _assign_string_to_pointer(admDocumentStr, output);
-  } catch(const std::exception& e) {
-    std::cerr << "Error: " << e.what() << std::endl;
-    return 1;
-  }
-  return 0;
-}
-
-
-int _create_renderer(void** renderer, const std::string& inputFilePath, const std::string& outputDirectory) {
-  try {
-    const std::string outputLayout("0+2+0"); // TODO: get it from args
-
-    auto bw64File = openBw64File(inputFilePath);
-
-    *renderer = new Renderer(bw64File, outputLayout, outputDirectory);
-  } catch(const std::exception& e) {
-    std::cerr << "Error: " << e.what() << std::endl;
-    return 1;
-  }
-  return 0;
-}
-
-int _set_element_to_render(void** renderer, const std::string& elementId) {
-  try {
-    Renderer* r = static_cast<Renderer*>(*renderer);
-    r->setElementToRender(elementId);
-  } catch(const std::exception& e) {
-    std::cerr << "Error: " << e.what() << std::endl;
-    return 1;
-  }
-  return 0;
-}
-
-int _set_element_gain(void** renderer, const std::string& elementId, const float gain) {
-  try {
-    Renderer* r = static_cast<Renderer*>(*renderer);
-    r->setElementGain(elementId, gain);
-  } catch(const std::exception& e) {
-    std::cerr << "Error: " << e.what() << std::endl;
-    return 1;
-  }
-  return 0;
-}
-
-int _process_rendering(void** renderer) {
-  try {
-    Renderer* r = static_cast<Renderer*>(*renderer);
-    r->process();
-  } catch(const std::exception& e) {
-    std::cerr << "Error: " << e.what() << std::endl;
-    return 1;
-  }
-  return 0;
-}
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -97,23 +35,54 @@ extern "C" {
  */
 
 int dump_bw64_adm(const char* source_path, const char** output) {
-  return _dump_bw64_adm_file(source_path, output);
+  try {
+    auto bw64File = bw64::readFile(source_path);
+
+    const std::string admDocumentStr = getAdmDocumentAsString(getAdmDocument(parseAdmXmlChunk(bw64File)));
+
+    _assign_string_to_pointer(admDocumentStr, output);
+  } catch(const std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    // TODO handle errors
+    return 1;
+  }
+  return 0;
 }
 
-int initialize_renderer(void** renderer, const char* source_path, const char* destination_path) {
-  return _create_renderer(renderer, source_path, destination_path);
-}
+typedef struct ElementGainEntry {
+    const char* element_id;
+    float gain;
+} ElementGainEntry;
 
-int set_element_to_render(void** renderer, const char* element_id) {
-  return _set_element_to_render(renderer, element_id);
-}
 
-int set_element_gain(void** renderer, const char* element_id, const float gain) {
-  return _set_element_gain(renderer, element_id, gain);
-}
+int process_rendering(const char* source_path,
+                      const char* destination_path,
+                      const char* element_id,
+                      const ElementGainEntry* gain_mapping,
+                      const unsigned int gain_mapping_size) {
+  try {
+    auto bw64File = bw64::readFile(source_path);
 
-int process_rendering(void** renderer) {
-  return _process_rendering(renderer);
+    const std::string outputLayout("0+2+0"); // TODO: get it from args
+    Renderer renderer(bw64File, outputLayout, destination_path);
+
+    if (gain_mapping != NULL && gain_mapping_size != 0) {
+      for (int i = 0; i < gain_mapping_size; ++i) {
+        const ElementGainEntry gain_entry = gain_mapping[i];
+        renderer.setElementGain(gain_entry.element_id, gain_entry.gain);
+      }
+    }
+
+    if (element_id != NULL) {
+      renderer.setElementToRender(element_id);
+    }
+
+    renderer.process();
+  } catch(const std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return 1;
+  }
+  return 0;
 }
 
 
