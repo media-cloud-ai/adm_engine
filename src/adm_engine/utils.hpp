5271aae802f14da6491b53250556788128955785
Valentin NOEL <valentin.noel@media-io.com>
2021-05-21 17:11:35 +0200

    Revert "Add utility function to open bw64 files"
    This reverts commit 11f391877ec8c6eb791ba13d1290f6abc81d3249.

--- src/adm_engine/utils.hpp
+++ src/adm_engine/utils.hpp
@@ -1,12 +1,9 @@
 #pragma once
 
 #include <string>
-#include <bw64/bw64.hpp>
 
 namespace admengine {
 
 std::string replaceSpecialCharacters(std::string& text);
-
-std::unique_ptr<bw64::Bw64Reader> openBw64File(const std::string& filePath);
 
 }
